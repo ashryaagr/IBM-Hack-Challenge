a6c36cdd80b8303c501d11e03c4eb21d20cea0cc
RUnmesh <unmeshr.18@gmail.com>
2019-07-11 01:12:32 +0530

    Progress on friends

--- display/src/models/friend.js
+++ display/src/models/friend.js
@@ -1,1 +1,19 @@
 // TODO: Write a model for the friend
+
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema({
+    name : {
+        type : String ,
+        required : true
+    },
+    owner : Schema.ObjectId,
+    usernames : {
+        type : Map,
+        of : String
+    } 
+});
+
+const friend = mongoose.model('Friend', friendSchema);
+
+module.exports = friend ;
--- display/src/routers/friend.js
+++ display/src/routers/friend.js
@@ -1,1 +1,40 @@
 // TODO : Add CRUD operations for the friend  model
+const Friend = require('../models/friend') ;
+const User = require('../models/user') ;
+const express = require('express');
+const passport = require('../passport') ;
+
+const router = new express.Router()
+
+router.post('/friend' , passport.authenticate('jwt', { session:false }) , 
+    async (req , res) => {
+        const friend = new Friend(req.body);
+        friend.save().then((friend)=>{
+            User.findById(friend.owner , function(err , user){
+                if (err)
+                    res.status(400).send(err);
+                else{
+                    user.friends.push(friend);
+                    user.save();
+                }
+            })
+            }).catch((e) => {
+                res.status(400).send(err);
+            })
+    }
+)
+
+router.post('/friend' , passport.authenticate('jwt', { session:false }) ,
+    async (req , res) => {
+        try{
+            Friend.findOne({name : req.body.name , owner : req.user._id} , function(err , friend){
+                if (err)
+                    throw err;
+                else
+                    friend.remove();
+            })
+        }  catch (e) {
+			res.status(500).send(e)
+		}
+    }
+)

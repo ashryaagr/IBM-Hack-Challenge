1b20c58e22d98004e1fb42ede0f5ad0dc1e1a37f
RUnmesh <unmeshr.18@gmail.com>
2019-07-11 01:16:08 +0530

    Progress on friends

--- display/src/routers/friend.js
+++ display/src/routers/friend.js
@@ -12,9 +12,9 @@ router.post('/friend' , passport.authenticate('jwt', { session:false }) ,
         friend.save().then((friend)=>{
             User.findById(friend.owner , function(err , user){
                 if (err)
-                    res.status(400).send(err);
+                    throw err
                 else{
-                    user.friends.push(friend);
+                    user.friends.push(friend); //check (trying to add friend to user's list of friends)
                     user.save();
                 }
             })
@@ -24,14 +24,38 @@ router.post('/friend' , passport.authenticate('jwt', { session:false }) ,
     }
 )
 
-router.post('/friend' , passport.authenticate('jwt', { session:false }) ,
+router.patch('/friend' , passport.authenticate('jwt', { session:false }) ,
+    async (req , res) => {
+        const usernames = req.body.usernames;
+        try{
+            Friend.findOne({name : req.body.name , owner : req.user._id} , function(err , friend){
+                if (err)
+                    throw err;
+                else
+                {
+                    usernames.forEach((username) => {
+                        friend.usernames.set(username.site , username.credentials);
+                        friend.save();
+                    });        
+                }
+            })
+        }  catch (e) {
+			res.status(500).send(e)
+		}
+    }
+)
+
+router.delete('/friend' , passport.authenticate('jwt', { session:false }) ,
     async (req , res) => {
         try{
             Friend.findOne({name : req.body.name , owner : req.user._id} , function(err , friend){
                 if (err)
                     throw err;
                 else
+                {
                     friend.remove();
+                    req.user.friends.pull(friend); //check (trying to remove friend from user's list of friends) 
+                }
             })
         }  catch (e) {
 			res.status(500).send(e)

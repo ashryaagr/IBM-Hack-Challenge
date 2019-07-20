467c96b21c05251c509924c21a381da9a9c35513
ashryaagr <ashryaagr@gmail.com>
2019-07-20 12:04:24 +0530

    initial setup for docker

--- display/src/models/friend.js
+++ display/src/models/friend.js
@@ -55,73 +55,77 @@ const friendSchema = new mongoose.Schema({
 
 friendSchema.pre('save', async function (next) {
 	const friend = this ;
-	const post = {
-		json: {
-			'usernames': JSON.parse(JSON.stringify(friend.usernames)),
-			'id': friend.id
-		},
-	};
-	function doRequest() {
-		return new Promise(function (resolve, reject) {
-			request.post(process.env.FLASK_URL, post, function (error, res, body) {
-				if (!error && (body.split(' ').length > 100)) {
-					resolve(body);
-				} else if (error) {
-					reject(error);
-				} else {
-					reject("Not sufficient data to analyze")
-				}
+	if (!fs.existsSync(path.join(__dirname, '../../../cache', ''.concat(friend._id, '.txt')))) {
+		const post = {
+			json: {
+				'usernames': JSON.parse(JSON.stringify(friend.usernames)),
+				'id': friend.id
+			},
+		};
+
+		function doRequest() {
+			return new Promise(function (resolve, reject) {
+				request.post(process.env.FLASK_URL, post, function (error, res, body) {
+					if (!error && (body.split(' ').length > 100)) {
+						resolve(body);
+					} else if (error) {
+						reject(error);
+					} else {
+						reject("Not sufficient data to analyze")
+					}
+				});
 			});
+		}
+
+		await doRequest();
+		var data = fs.readFileSync(path.join(__dirname, '../../../cache', ''.concat(friend._id, '.txt')), 'utf-8');
+		const profileParams = {
+			content: data.toString(),
+			content_type: 'text/plain',
+			consumption_preferences: true,
+			raw_scores: true,
+		};
+		personalityInsights.profile(profileParams)
+			.then(profile => {
+				fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-personality.json')), JSON.stringify(profile), (err) => {
+					if (err) throw Error(err.message)
+				});
+			}).catch(err => {
+			throw Error(err.message)
+		});
+		const analyzeParams = {
+			'text': data.toString(),
+			'features': {
+				'categories': {
+					'limit': 10
+				},
+			}
+		};
+		naturalLanguageUnderstanding.analyze(analyzeParams)
+			.then(analysisResults => {
+				fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-nlu.json')), JSON.stringify(analysisResults), (err) => {
+					if (err) throw Error(err.message)
+				});
+			})
+			.catch(err => {
+				throw Error(err.message)
+			});
+		const toneParams = {
+			tone_input: {
+				'text': data.toString(),
+			},
+			content_type: 'text/plain',
+			sentences: false,
+		};
+		toneAnalyzer.tone(toneParams)
+			.then(toneAnalysis => {
+				fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-tone.json')), JSON.stringify(toneAnalysis), (err) => {
+					if (err) throw Error(err.message)
+				});
+			}).catch(err => {
+			throw Error(err.message)
 		});
 	}
-	await  doRequest();
-	var data = fs.readFileSync(path.join(__dirname, '../../../cache', ''.concat(friend._id, '.txt')), 'utf-8') ;
-	const profileParams = {
-		content: data.toString() ,
-		content_type: 'text/plain',
-		consumption_preferences: true,
-		raw_scores: true,
-	};
-	personalityInsights.profile(profileParams)
-		.then(profile => {
-			fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-personality.json')), JSON.stringify(profile), (err) => {
-				if (err) throw Error(err.message)
-			});
-		}).catch(err => {
-			throw Error(err.message)
-		});
-	const analyzeParams = {
-		'text': data.toString(),
-		'features': {
-			'categories': {
-				'limit': 10
-			},
-		}
-	};
-	naturalLanguageUnderstanding.analyze(analyzeParams)
-		.then(analysisResults => {
-			fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-nlu.json')), JSON.stringify(analysisResults), (err) => {
-				if (err) throw Error(err.message)
-			});
-		})
-		.catch(err => {
-			throw Error(err.message)
-		});
-	const toneParams = {
-		tone_input: {
-		'text' : data.toString(),
-		},
-		content_type: 'text/plain',
-		sentences: false,
-	};
-	toneAnalyzer.tone(toneParams)
-		.then(toneAnalysis => {
-			fs.writeFile(path.join(__dirname, '../../../cache', ''.concat(friend._id, '-tone.json')), JSON.stringify(toneAnalysis), (err) => {
-				if (err) throw Error(err.message)
-			});
-		}).catch(err => {
-			throw Error(err.message)
-		});
 	next();
 });
 
